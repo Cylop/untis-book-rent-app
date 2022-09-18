5e3c15e0fd5a7b67e839e37571e9433f3bbe3a62
Nicholas Petrasek <nicholas.petrasek@gmail.com>
2022-09-18 12:51:52 +0200

    changed login fields to email and password

--- lib/ui/pages/login/bloc/login_bloc.dart
+++ lib/ui/pages/login/bloc/login_bloc.dart
@@ -1,6 +1,12 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:injectable/injectable.dart';
+import 'package:untis_book_rent_app/injection.dart';
 import 'package:untis_book_rent_app/ui/pages/login/models/models.dart';
+import 'package:untis_book_rent_app/ui/state/auth_bloc/bloc.dart';
+import 'package:untis_book_rent_app/ui/state/auth_bloc/event.dart';
+import 'package:untis_book_rent_app/ui/state/auth_bloc/state.dart';
 import 'package:untis_book_rent_app/ui/state/repositories/auth_repository.dart';
 
 import 'package:formz/formz.dart';
@@ -8,27 +14,25 @@ import 'package:formz/formz.dart';
 part 'login_event.dart';
 part 'login_state.dart';
 
+@injectable
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
-  LoginBloc({
-    required IAuthenticationRepository authenticationRepository,
-  })  : _authenticationRepository = authenticationRepository,
-        super(const LoginState()) {
-    on<LoginUsernameChanged>(_onUsernameChanged);
+  final IAuthenticationRepository _authenticationRepository;
+
+  LoginBloc(this._authenticationRepository) : super(const LoginState()) {
+    on<LoginEmailChanged>(_onEmailChanged);
     on<LoginPasswordChanged>(_onPasswordChanged);
     on<LoginSubmitted>(_onSubmitted);
   }
 
-  final IAuthenticationRepository _authenticationRepository;
-
-  void _onUsernameChanged(
-    LoginUsernameChanged event,
+  void _onEmailChanged(
+    LoginEmailChanged event,
     Emitter<LoginState> emit,
   ) {
-    final username = Username.dirty(event.username);
+    final email = Email.dirty(event.email);
     emit(
       state.copyWith(
-        username: username,
-        status: Formz.validate([state.password, username]),
+        email: email,
+        status: Formz.validate([state.password, email]),
       ),
     );
   }
@@ -41,7 +45,7 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
     emit(
       state.copyWith(
         password: password,
-        status: Formz.validate([password, state.username]),
+        status: Formz.validate([password, state.email]),
       ),
     );
   }
@@ -53,12 +57,17 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
     if (state.status.isValidated) {
       emit(state.copyWith(status: FormzStatus.submissionInProgress));
       try {
+        /*var authBloc = locator<AuthenticationBloc>();
+        authBloc.add(AuthenticationLoginRequest(
+            email: state.email.value, password: state.password.value));*/
         await _authenticationRepository.logIn(
-          username: state.username.value,
+          email: state.email.value,
           password: state.password.value,
         );
         emit(state.copyWith(status: FormzStatus.submissionSuccess));
-      } catch (_) {
+      } catch (error) {
+        debugPrint("Error for submission");
+        debugPrint(error.toString());
         emit(state.copyWith(status: FormzStatus.submissionFailure));
       }
     }
