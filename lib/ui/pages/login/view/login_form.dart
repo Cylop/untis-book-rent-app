63527851e1b482e3eebee2d1efa439503e311f33
Nicholas Petrasek <nicholas.petrasek@gmail.com>
2022-09-18 12:52:14 +0200

    adapted login form

--- lib/ui/pages/login/view/login_form.dart
+++ lib/ui/pages/login/view/login_form.dart
@@ -39,15 +39,16 @@ class _UsernameInput extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<LoginBloc, LoginState>(
-      buildWhen: (previous, current) => previous.username != current.username,
+      buildWhen: (previous, current) => previous.email != current.email,
       builder: (context, state) {
         return TextField(
-          key: const Key('loginForm_usernameInput_textField'),
+          key: const Key('loginForm_emailInput_textField'),
           onChanged: (username) =>
-              context.read<LoginBloc>().add(LoginUsernameChanged(username)),
+              context.read<LoginBloc>().add(LoginEmailChanged(username)),
           decoration: InputDecoration(
-            labelText: 'username',
-            errorText: state.username.invalid ? 'invalid username' : null,
+            labelText: 'Email',
+            errorText:
+                state.email.invalid ? 'Please provide a valid Email' : null,
           ),
         );
       },
@@ -67,8 +68,8 @@ class _PasswordInput extends StatelessWidget {
               context.read<LoginBloc>().add(LoginPasswordChanged(password)),
           obscureText: true,
           decoration: InputDecoration(
-            labelText: 'password',
-            errorText: state.password.invalid ? 'invalid password' : null,
+            labelText: 'Password',
+            errorText: state.password.invalid ? 'Password is required' : null,
           ),
         );
       },
