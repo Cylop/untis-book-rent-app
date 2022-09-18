5a5e3d23f6d6bf4f6e70393e00ec9c96aecee77a
Nicholas Petrasek <nicholas.petrasek@gmail.com>
2022-09-18 12:48:05 +0200

    adapted respositories

--- lib/ui/state/repositories/auth_repository.dart
+++ lib/ui/state/repositories/auth_repository.dart
@@ -1,34 +1,56 @@
 import 'dart:async';
 
+import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
+import 'package:untis_book_rent_app/api/dto/user/user.dart';
+import 'package:untis_book_rent_app/api/rest/services/auth_service.dart';
 
-enum AuthenticationStatus { unknown, authenticated, unauthenticated }
+enum AuthenticationStatus {
+  unknown,
+  loggingIn,
+  authenticated,
+  loggingOut,
+  unauthenticated
+}
 
-@Injectable(as: IAuthenticationRepository)
+@Singleton(as: IAuthenticationRepository)
 class AuthenticationRepository implements IAuthenticationRepository {
+  final AuthService _authService;
   final _controller = StreamController<AuthenticationStatus>();
+
+  AuthenticationRepository(this._authService);
 
   @override
   Stream<AuthenticationStatus> get status async* {
-    await Future<void>.delayed(const Duration(seconds: 1));
+    await Future<void>.delayed(const Duration(milliseconds: 400));
     yield AuthenticationStatus.unauthenticated;
     yield* _controller.stream;
   }
 
   @override
-  Future<void> logIn({
-    required String username,
+  Future<StateUser> logIn({
+    required String email,
     required String password,
   }) async {
-    await Future.delayed(
-      const Duration(milliseconds: 300),
-      () => _controller.add(AuthenticationStatus.authenticated),
-    );
+    _controller.add(AuthenticationStatus.loggingIn);
+    try {
+      var user = await _authService
+          .login(SignInUserDto(email: email, password: password));
+      _controller.add(AuthenticationStatus.authenticated);
+      return StateUser.fromUser(user);
+    } catch (error) {
+      debugPrint("Login error");
+      debugPrint(error.toString());
+      _controller.add(AuthenticationStatus.unauthenticated);
+    }
+
+    return StateUser.empty;
   }
 
   @override
-  void logOut() {
+  Future<void> logOut() {
     _controller.add(AuthenticationStatus.unauthenticated);
+    return _authService.logout();
   }
 
   @override
@@ -38,8 +60,8 @@ class AuthenticationRepository implements IAuthenticationRepository {
 abstract class IAuthenticationRepository {
   Stream<AuthenticationStatus> get status;
 
-  Future<void> logIn({required String username, required String password});
-  void logOut();
+  Future<StateUser> logIn({required String email, required String password});
+  Future<void> logOut();
 
   void dispose();
 }
--- lib/ui/state/repositories/user_repository.dart
+++ lib/ui/state/repositories/user_repository.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'package:injectable/injectable.dart';
 import 'package:untis_book_rent_app/api/dto/user/user.dart';
 
-@Injectable(as: IUserRepository)
+@Singleton(as: IUserRepository)
 class UserRepository implements IUserRepository {
   StateUser? _user;
 
