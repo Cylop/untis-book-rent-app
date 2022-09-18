e052d81ef1d43c3582211ee3e16d46cde32805ee
Nicholas Petrasek <nicholas.petrasek@gmail.com>
2022-09-18 12:49:16 +0200

    adapted routes

--- lib/ui/routing/router.dart
+++ lib/ui/routing/router.dart
@@ -1,18 +1,23 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:untis_book_rent_app/app.dart';
+import 'package:untis_book_rent_app/ui/pages/home/home.dart';
 import 'package:untis_book_rent_app/ui/pages/login/view/login_page.dart';
 
 @MaterialAutoRouter(
   replaceInRouteName: 'Page,Route',
   routes: [
     AutoRoute(
-      path: '',
+      path: '/',
       page: AppView,
       initial: true,
     ),
     AutoRoute(
-      path: '/login',
+      path: '/',
       page: LoginPage,
+    ),
+    AutoRoute(
+      path: '/',
+      page: HomePage,
     ),
   ],
 )
--- lib/ui/routing/router.gr.dart
+++ lib/ui/routing/router.gr.dart
@@ -11,49 +11,61 @@
 // ignore_for_file: type=lint
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:auto_route/auto_route.dart' as _i3;
-import 'package:flutter/material.dart' as _i4;
+import 'package:auto_route/auto_route.dart' as _i4;
+import 'package:flutter/material.dart' as _i5;
 
 import '../../app.dart' as _i1;
+import '../pages/home/home.dart' as _i3;
 import '../pages/login/view/login_page.dart' as _i2;
 
-class AppRouter extends _i3.RootStackRouter {
-  AppRouter([_i4.GlobalKey<_i4.NavigatorState>? navigatorKey])
+class AppRouter extends _i4.RootStackRouter {
+  AppRouter([_i5.GlobalKey<_i5.NavigatorState>? navigatorKey])
       : super(navigatorKey);
 
   @override
-  final Map<String, _i3.PageFactory> pagesMap = {
+  final Map<String, _i4.PageFactory> pagesMap = {
     AppView.name: (routeData) {
-      return _i3.MaterialPageX<dynamic>(
+      return _i4.MaterialPageX<dynamic>(
           routeData: routeData, child: const _i1.AppView());
     },
     LoginRoute.name: (routeData) {
-      return _i3.MaterialPageX<dynamic>(
+      return _i4.MaterialPageX<dynamic>(
           routeData: routeData, child: const _i2.LoginPage());
+    },
+    HomeRoute.name: (routeData) {
+      return _i4.MaterialPageX<dynamic>(
+          routeData: routeData, child: const _i3.HomePage());
     }
   };
 
   @override
-  List<_i3.RouteConfig> get routes => [
-        _i3.RouteConfig('/#redirect',
-            path: '/', redirectTo: '', fullMatch: true),
-        _i3.RouteConfig(AppView.name, path: ''),
-        _i3.RouteConfig(LoginRoute.name, path: '/login')
+  List<_i4.RouteConfig> get routes => [
+        _i4.RouteConfig(AppView.name, path: '/'),
+        _i4.RouteConfig(LoginRoute.name, path: '/'),
+        _i4.RouteConfig(HomeRoute.name, path: '/')
       ];
 }
 
 /// generated route for
 /// [_i1.AppView]
-class AppView extends _i3.PageRouteInfo<void> {
-  const AppView() : super(AppView.name, path: '');
+class AppView extends _i4.PageRouteInfo<void> {
+  const AppView() : super(AppView.name, path: '/');
 
   static const String name = 'AppView';
 }
 
 /// generated route for
 /// [_i2.LoginPage]
-class LoginRoute extends _i3.PageRouteInfo<void> {
-  const LoginRoute() : super(LoginRoute.name, path: '/login');
+class LoginRoute extends _i4.PageRouteInfo<void> {
+  const LoginRoute() : super(LoginRoute.name, path: '/');
 
   static const String name = 'LoginRoute';
+}
+
+/// generated route for
+/// [_i3.HomePage]
+class HomeRoute extends _i4.PageRouteInfo<void> {
+  const HomeRoute() : super(HomeRoute.name, path: '/');
+
+  static const String name = 'HomeRoute';
 }
