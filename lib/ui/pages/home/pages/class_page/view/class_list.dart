125dd8ff1c226459650369b8441aca9ee44fca73
Nicholas Petrasek <nicholas.petrasek@gmail.com>
2022-09-23 13:39:28 +0200

    implemented generic bloc list in class_page

--- lib/ui/pages/home/pages/class_page/view/class_list.dart
+++ lib/ui/pages/home/pages/class_page/view/class_list.dart
@@ -1,89 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
-import 'package:untis_book_rent_app/ui/pages/home/pages/book_page/widgets/bottom_loader.dart';
-import 'package:untis_book_rent_app/ui/pages/home/pages/class_page/class_bloc/bloc.dart';
+import 'package:untis_book_rent_app/api/dto/class/class.dart';
+import 'package:untis_book_rent_app/injection.dart';
 import 'package:untis_book_rent_app/ui/pages/home/pages/class_page/widgets/class_list_tile.dart';
-import 'package:untis_book_rent_app/ui/pages/home/widgets/reached_list_end.dart';
+import 'package:untis_book_rent_app/ui/pages/home/widgets/animated_infinite_scrolling_list/list.dart';
 
-class ClassList extends StatefulWidget {
+class ClassList extends StatelessWidget {
   const ClassList({Key? key}) : super(key: key);
 
   @override
-  State<ClassList> createState() => _ClassListState();
-}
-
-class _ClassListState extends State<ClassList> {
-  final _scrollController = ScrollController();
-
-  @override
-  void initState() {
-    super.initState();
-    _scrollController.addListener(_onScroll);
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return BlocBuilder<ClassBloc, ClassState>(builder: (context, state) {
-      switch (state.status) {
-        case ClassStatus.failure:
-          return const Center(child: Text('Klassen konnten nicht geladen werden'));
-        case ClassStatus.success:
-          if (state.classes.isEmpty) {
-            return const Center(child: Text('Keine Klassen vorhanden'));
-          }
-          return AnimationLimiter(
-            child: ListView.separated(
-              padding: const EdgeInsets.all(10),
-              shrinkWrap: true,
-              itemBuilder: (BuildContext context, int index) {
-                return index >= state.classes.length
-                    ? state.hasReachedMax
-                        ? const ReachedEnd(
-                            message: 'Alle Klassen geladen',
-                          )
-                        : const BottomLoader()
-                    : AnimationConfiguration.staggeredList(
-                        position: index,
-                        duration: const Duration(milliseconds: 500),
-                        child: SlideAnimation(
-                          verticalOffset: 50.0,
-                          child: FadeInAnimation(
-                            child: ClassListTile(schoolClass: state.classes[index]),
-                          ),
-                        ),
-                      );
-              },
-              separatorBuilder: (BuildContext context, int index) =>
-                  const SizedBox(
-                height: 10,
-              ),
-              itemCount: state.classes.length + 1,
-              controller: _scrollController,
-            ),
-          );
-        case ClassStatus.initial:
-          return const Center(child: CircularProgressIndicator());
-      }
-    });
-  }
-
-  @override
-  void dispose() {
-    _scrollController
-      ..removeListener(_onScroll)
-      ..dispose();
-    super.dispose();
-  }
-
-  void _onScroll() {
-    if (_isBottom) context.read<ClassBloc>().add(ClassFetched());
-  }
-
-  bool get _isBottom {
-    if (!_scrollController.hasClients) return false;
-    final maxScroll = _scrollController.position.maxScrollExtent;
-    final currentScroll = _scrollController.offset;
-    return currentScroll >= (maxScroll * 0.9);
+    return AnimatedInfiniteScrollingBlocList<SchoolClass>(
+      entityName: 'Classes',
+      bloc: locator(instanceName: 'animatedBookListBloc'),
+      builder: (context, entity) => ClassListTile(schoolClass: entity),
+    );
   }
 }
--- lib/ui/pages/home/pages/class_page/view/class_page.dart
+++ lib/ui/pages/home/pages/class_page/view/class_page.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:untis_book_rent_app/injection.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:untis_book_rent_app/ui/pages/home/pages/class_page/class_bloc/bloc.dart';
 import 'package:untis_book_rent_app/ui/pages/home/pages/class_page/view/class_list.dart';
 
 class ClassPage extends StatefulWidget {
@@ -15,28 +12,25 @@ class ClassPage extends StatefulWidget {
 class _ClassPageState extends State<ClassPage> {
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => locator<ClassBloc>()..add(ClassFetched()),
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Books'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () async {
-            /*await showModalBottomSheet<String>(
-              context: context,
-              builder: (BuildContext builder) {
-                return const CreateBookModal();
-              },
-            );*/
-          },
-          mini: true,
-          child: const Icon(Icons.add),
-          //icon: const Icon(Icons.add), label: const Text('Create Book'),
-          //label: ,
-        ),
-        body: const ClassList(),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Classes'),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          /*await showModalBottomSheet<String>(
+            context: context,
+            builder: (BuildContext builder) {
+              return const CreateBookModal();
+            },
+          );*/
+        },
+        mini: true,
+        child: const Icon(Icons.add),
+        //icon: const Icon(Icons.add), label: const Text('Create Book'),
+        //label: ,
+      ),
+      body: const ClassList(),
     );
   }
 }
